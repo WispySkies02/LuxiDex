00508ace34d606ba76a5c09474197df2c7a2fdf1
WispySkies02 <waggonernick02@gmail.com>
2022-11-25 00:19:20 -0500

    Added the invite command.

---
+++ commands/invite.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'invite',
+    execute(message, args) {
+        let invitelink = new MessageEmbed()
+            .setTitle('Invite Floofy Fluff')
+            .setColor('GREEN')
+            .setDescription(
+      `You can invite me by clicking [here](https://discord.com/oauth2/authorize?client_id=713542823454048277&scope=bot%20applications.commands&permissions=2147483647)`);
+  message.channel.send(invitelink);
+    },
+};
