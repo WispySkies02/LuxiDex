5e05c2e77b55e96e33ec0c00f16bf0e5477d5b05
NooblysVR <47481465+NooblysVR@users.noreply.github.com>
2022-11-25 06:25:10 +0100

    invite: added aliases to the command.

--- commands/invite.js
+++ commands/invite.js
@@ -1,11 +1,12 @@
 module.exports = {
     name: 'invite',
+    aliases: ['inv'],
     execute(message, args) {
         let invitelink = new MessageEmbed()
             .setTitle('Invite Floofy Fluff')
             .setColor('GREEN')
             .setDescription(
       `You can invite me by clicking [here](https://discord.com/oauth2/authorize?client_id=713542823454048277&scope=bot%20applications.commands&permissions=2147483647)`);
-  message.channel.send(invitelink);
+    message.channel.send(invitelink);
     },
 };
