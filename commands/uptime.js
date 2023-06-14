cf43b5abb7015cb2076d0fdde7f47ca05e4f456c
WispySkies02 <waggonernick02@gmail.com>
2023-06-14 19:12:02 -0400

    Changed the code to a basic text reply & fixed it

--- commands/uptime.js
+++ commands/uptime.js
@@ -4,22 +4,13 @@ module.exports = {
     name: 'uptime',
     aliases: ['time'],
 
-    execute(client, message, cmd, args, Discord) {
+    execute(message, args, client) {
 
         let days = Math.floor(client.uptime / 86400000)
         let hours = Math.floor(client.uptime / 3600000) % 24
         let minutes = Math.floor(client.uptime / 60000) % 60
         let seconds = Math.floor(client.uptime / 1000) % 60
 
-        let upEmbed = new MessageEmbed()
-            .setColor("BLUE")
-            .setDescription(`I have been online for \`${days}\` days, \`${hours}\` hours, \`${minutes}\` minutes, and \`${seconds}\` seconds.`);
-
-            
-            console.log(!message)
-            console.log(!message.channel)
-            console.log(message)
-
-            message.channel.send(upEmbed);
+            message.channel.send(`I have been online for ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds.`);
     },
 };
