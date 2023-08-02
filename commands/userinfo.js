b48454375ae9e2c0df243270a6e471b4d9e2fff8
WispySkies02 <waggonernick02@gmail.com>
2023-08-02 14:54:15 -0400

    Added userinfo.js and updated packsges & index.js

---
+++ commands/userinfo.js
@@ -0,0 +1,56 @@
+const { MessageEmbed } = require('discord.js');
+const moment = require('moment');
+
+module.exports = {
+  name: 'userinfo',
+  run: async (client, message, args, guild) => {
+    const member = message.mentions.members?.first() || message.guild.members.cache.get(args[0]) || message.member;
+    if (!member) return message.channel.send('Please mention the user for the userinfo..');
+
+    const userFlags = (await member.user.fetchFlags()).toArray();
+    const activities = [];
+    let customStatus;
+
+    for (const activity of member.presence.activities.values()) {
+      switch (activity.type) {
+        case 'PLAYING':
+          activities.push(`Playing **${activity.name}**`);
+          break;
+        case 'LISTENING':
+          if (member.user.bot) activities.push(`Listening to **${activity.name}**`);
+          else activities.push(`Listening to **${activity.details}** by **${activity.state}**`);
+          break;
+        case 'WATCHING':
+          activities.push(`Watching **${activity.name}**`);
+          break;
+        case 'STREAMING':
+          activities.push(`Streaming **${activity.name}**`);
+          break;
+        case 'CUSTOM_STATUS':
+          customStatus = activity.state;
+          break;
+      }
+    }
+
+    const uiembed = new MessageEmbed()
+      .setTitle(`${member.displayName}'s Information`)
+      .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+      .addField('User', member, true)
+      .addField('Discriminator', `\`#${member.user.discriminator}\``, true)
+      .addField('ID', `\`${member.id}\``, true)
+      .addField('Status', member.presence.status, true)
+      .addField('Bot', `\`${member.user.bot}\``, true)
+      .addField('Color Role', member.roles.color || '`None`', true)
+      .addField('Highest Role', member.roles.highest, true)
+      .addField('Joined server on', `\`${moment(member.joinedAt).format('MMM DD YYYY')}\``, true)
+      .addField('Joined Discord on', `\`${moment(member.user.createdAt).format('MMM DD YYYY')}\``, true)
+      .setFooter(message.member.displayName, message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor(member.displayHexColor);
+
+    if (activities.length > 0) uiembed.setDescription(activities.join('\n'));
+    if (customStatus) uiembed.spliceFields(0, 0, { name: 'Custom Status', value: customStatus });
+
+    message.channel.send(uiembed);
+  },
+};
--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-const { Client, Collection } = require('discord.js'); // importing discord.js and deconstructing it into just Client.
-
-let bot = new Client({ // client declaration.
-  fetchAllMembers: true, // Remove this if the bot is in large guilds.
+const { Client, Collection, MessageEmbed } = require('discord.js');
+const bot = new Client({
+  fetchAllMembers: true,
   presence: {
     status: 'online',
     activity: {
       name: `Coded by WispySkies02`,
-      type: 'PLAYING'
-    }
-  }
+      type: 'PLAYING',
+    },
+  },
 });
 
 const config = require('./config');
-//const commands = require('./help');
 bot.commands = new Collection();
 const fs = require('fs');
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
-  // set a new item in the Collection
-  // with the key as the command name and the value as the exported module
-  console.log(`Loaded command: ${command.name}`)
+  console.log(`Loaded command: ${command.name}`);
   bot.commands.set(command.name, command);
 }
+
 require('dotenv').config();
 
 bot.on('message', message => {
@@ -30,20 +27,18 @@ bot.on('message', message => {
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/);
   const commandName = args.shift().toLowerCase();
-  
-  const command = bot.commands.get(commandName) 
-    || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+  const command = bot.commands.get(commandName) || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
   if (!command) return;
 
   try {
-      command.execute(message, args, bot);
+    command.run(bot, message, args); // Pass the 'bot', 'message', and 'args' objects to the command
   } catch (error) {
-      console.error(error);
-      message.reply('there was an error trying to execute that command!');
+    console.error(error);
+    message.reply('something went wrong trying to display this command. Try again or contact support!');
   }
 });
-
 
 bot.on('ready', () => console.log(`Logged in as ${bot.user.tag}.`));
 
