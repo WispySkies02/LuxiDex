5eac5e6c8294170244ebe12d2d009462ee80d4dc
WispySkies02 <waggonernick02@gmail.com>
2023-08-08 22:47:13 -0400

    Added repeat command description for the

--- commands/fun/repeat.js
+++ commands/fun/repeat.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'repeat',
     aliases: ['r'],
+    desription: 'Repeats whatever is said',
     async execute(message, args) {
       if (args.length > 0)
         message.channel.send(args.join(' '));
